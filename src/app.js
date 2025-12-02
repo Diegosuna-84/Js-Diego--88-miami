4e4449eda50a54fac2410ae591bdc0a49fe32743
Diego Osuna <7rgsx7d6jz@privaterelay.appleid.com>
2025-12-02 02:21:05 +0000

    monday921

--- src/app.js
+++ src/app.js
@@ -5,7 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = ["before the class", "when I was sleeping", "while I was exercising", "during my lunch", "while I was praying"];
+  const excuseLine = document.querySelector("#excuse");
+  function getrandomindex(array) {
+    const randomindex = Math.floor(Math.random() * array.length);
+    return randomindex;
+  }
+  let whoindex = getrandomindex(who);
+  let actionindex = getrandomindex(action);
+  let whatindex = getrandomindex(what);
+  let whenindex = getrandomindex(when);
+  excuseLine.innerHTML = `${who[whoindex]} ${action[actionindex]} ${what[whatindex]} ${when[whenindex]}`;
+
+
+  console.log(`${what} ${who} ${when}`);
 };
