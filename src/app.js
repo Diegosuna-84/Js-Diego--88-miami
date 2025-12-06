471929719c16435cce253ef145a7bdc0208a2351
Diego Osuna <dieouna@gmail.com>
2025-12-05 19:15:23 -0500

    final project

--- src/app.js
+++ src/app.js
@@ -20,9 +20,11 @@ window.onload = function () {
     "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/1627720/capsule_616x353.jpg?t=1764565054",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhDiB5RB_xLJ0219yyRHoB7KQslkKbyi2-EQ&s",
     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu6lM8sP8YAP4WLyervrY7vdvSWPvvduF0LQ&s",
+    "https://insider-gaming.com/wp-content/uploads/2024/08/black-myth-wukong.png",
   ];
   const excuseLine = document.querySelector("#excuse");
   const picture = document.querySelector("#pictures");
+  // const footer = document.querySelector(#footer)
 
   function getrandomindex(array) {
     const randomindex = Math.floor(Math.random() * array.length);
@@ -39,4 +41,7 @@ window.onload = function () {
 
   picture.src = pictures[getrandomindex(pictures)];
   console.log(`${what} ${who} ${when} ${action}`);
+  // console.log(footer.classname)
+
+  // footer.classmate += " bg-success"
 };
